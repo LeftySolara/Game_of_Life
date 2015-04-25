fc463290576da8233cd7c02ff7daa94b0ce461cd
Jalen Adams <leftysolara@gmail.com>
2015-04-25 19:33:16 -0400

    add findNeighbors function and two function templates

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 - write rules for cells living/dying
 - add checks for input size during setInitialState()
 - add check for correct character values when initializing grid
-- make grid into a toroidal array so cells loop when moving out of bounds
 - add function to randomly generate an initial state, given grid dimensions
 - implement exceptions where marked
+- learn ncurses to implement graphics
 */
 
 #define ALIVE '#'
@@ -53,7 +53,34 @@ void setInitialState(bool** grid, ifstream& init_file, unsigned int grid_width, 
 }
 
 
-void display_grid(bool** grid, unsigned int grid_width, unsigned int grid_depth)
+void findNeighbors(bool* neighbors, bool** grid, unsigned int grid_width, unsigned int grid_depth, int target_y; int target_x)
+{
+    unsigned int count = 0;
+    for (int y = target_y - 1; y <= target_y + 1; ++y) {
+        for (int x = target_x - 1; x <= target_x + 1; ++x) {
+            if (y == target_y && x == target_x) // ignore target itself
+                continue
+            // add length of row/column to offset effect of negative int in mod calculation
+            neighbors[count] = grid[(y + grid_depth) % grid_depth][(x + grid_width) % grid_width];
+            ++count;
+        }
+    }
+}
+
+
+void writeNextFrame(bool** grid, unsigned int grid_width, unsigned int grid_depth)
+{
+    // call findNeighbors on each element in grid and change value based on Conway's rules
+    // create a second grid object to read from so calculations aren't affected by new frame being drawn
+}
+
+void runGame(unsigned int turns, bool** grid)
+{
+    // main game loop
+}
+
+
+void displayGrid(bool** grid, unsigned int grid_width, unsigned int grid_depth)
 {
     for (unsigned int i = 0; i < grid_depth; ++i) {
         for (unsigned int j = 0; j < grid_width; ++j) {
@@ -82,13 +109,13 @@ int main()
     // get dimensions of grid
     init_file >> grid_width >> grid_depth >> turns;
 
-    // move buffer position to next line
+    // move buffer position to next line in file
     init_file.seekg(1, init_file.cur);
 
     // create grid and set initial state
     bool** life_grid = createGridArray(grid_width,grid_depth);
     setInitialState(life_grid, init_file, grid_width, grid_depth);
-    display_grid(life_grid, grid_width, grid_depth);
+    displayGrid(life_grid, grid_width, grid_depth);
 
     init_file.close();
 
